28dd010ddb417983f071ed4a35b83959420fc0ed
Monika Šostkaitė <Monika.Sostkaite@adform.com>
2022-12-11 19:44:24 +0200

    Update login page unit tests

--- src/components/LoginPage/LoginPage.test.jsx
+++ src/components/LoginPage/LoginPage.test.jsx
@@ -5,40 +5,61 @@ import { render, screen } from '@testing-library/react';
 import LoginPage from './LoginPage';
 import { AuthContext } from '../../hooks/useAuth';
 
-test('render login form, fill required fields and submit', () => {
-    const login = jest.fn();
+const USERNAME_LABEL = 'Username *';
+const PASSWORD_LABEL = 'Password *';
+const LOGIN_BUTTON = 'logIn';
+const USERNAME = 'isun';
+const PASSWORD = 'passwrod';
+
+describe('LoginPage', () => {
+    const onLogin = jest.fn();
     const token = 'test';
 
     const value = {
         token,
-        login,
-        logout: jest.fn(),
+        onLogin,
+        onLogout: jest.fn(),
     };
 
-    render(
-        <AuthContext.Provider value={value}>
-            <LoginPage />
-        </AuthContext.Provider>
-    );
+    test('render login form and check if username, password and login button exists', () => {
+        render(
+            <AuthContext.Provider value={value}>
+                <LoginPage />
+            </AuthContext.Provider>
+        );
 
-    const usernameInput = screen.getByLabelText('Username *');
-    const passwordInput = screen.getByLabelText('Password *');
-    const logInButton = screen.getByTestId('logIn');
-    expect(usernameInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
+        const usernameInput = screen.getByLabelText(USERNAME_LABEL);
+        const passwordInput = screen.getByLabelText(PASSWORD_LABEL);
+        const logInButton = screen.getByTestId(LOGIN_BUTTON);
 
-    expect(usernameInput).toHaveValue('');
-    userEvent.type(usernameInput, 'isun');
+        expect(usernameInput).toBeInTheDocument();
+        expect(passwordInput).toBeInTheDocument();
+        expect(logInButton).toBeInTheDocument();
+    });
 
-    expect(usernameInput).toHaveValue('isun');
-    userEvent.tab();
+    test('render login form, fill required fields and submit', () => {
+        render(
+            <AuthContext.Provider value={value}>
+                <LoginPage />
+            </AuthContext.Provider>
+        );
 
-    userEvent.type(passwordInput, 'passwrod');
-    userEvent.tab();
+        const usernameInput = screen.getByLabelText(USERNAME_LABEL);
+        const passwordInput = screen.getByLabelText(PASSWORD_LABEL);
+        const logInButton = screen.getByTestId(LOGIN_BUTTON);
 
-    userEvent.click(logInButton);
-    expect(login).toHaveBeenCalledWith({
-        username: 'isun',
-        password: 'passwrod',
+        expect(usernameInput).toHaveValue('');
+        userEvent.type(usernameInput, USERNAME);
+        expect(usernameInput).toHaveValue(USERNAME);
+
+        expect(passwordInput).toHaveValue('');
+        userEvent.type(passwordInput, PASSWORD);
+        expect(passwordInput).toHaveValue(PASSWORD);
+
+        userEvent.click(logInButton);
+        expect(onLogin).toHaveBeenCalledWith({
+            username: USERNAME,
+            password: PASSWORD,
+        });
     });
 });
